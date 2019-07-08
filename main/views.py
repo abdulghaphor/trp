41eaa8555cff9546b0476ccf567ef484b31be9a5
Abdulghaphor Hajjieh <ghaphorhajjieh@gmail.com>
2019-07-09 01:32:04 +0300

    first commit

---
+++ main/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from .forms import ArticleForm
+from .models import Article
+
+def articles_list(request):
+    articles = Article.objects.all()
+    context = {
+        "articles" : articles,
+    }
+    return render(request, "articles_list.html", context)
+
+def article_details(request, article_id):
+    context = { "article" : Article.objects.get(id=article_id)}
+    return render(request, 'article_details.html', context)
+
+def create_article(request):
+	form = ArticleForm()
+	if request.method == "POST":
+		form = ArticleForm(request.POST)
+		if form.is_valid():
+			article = form.save(commit=False)
+			article.author = request.user
+			article.save()
+			return redirect('article-details', article.id)
+
+	context = {"form" : form}
+
+	return render(request, "create_article.html", context)
+
+def edit_article(request, article_id):
+	article = Article.objects.get(id=article_id)
+
+	form = ArticleForm(instance=article)
+	if request.method == "POST":
+		form = ArticleForm(request.POST, instance=article)
+
+		if form.is_valid():
+			form.save()
+			return redirect('article-details', article_id)
+
+	context = {"form":form, "article":article}
+	return render(request, 'edit_article.html', context)
+
+def my_articles_list(request):
+	return render(request, "my_articles_list.html")
---
+++ reading_potato/urls.py
@@ -0,0 +1,31 @@
+"""reading_potato URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from main import views
+from authentication import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('articles/', views.articles_list, name="articles-list"),
+    path('articles/<int:article_id>/', views.article_details, name="article-details"),
+	path('create/', views.create_article, name="create-article"),
+	path('edit/<int:article_id>', views.edit_article, name="edit-article"),
+	path('my-articles/', views.my_articles_list, name="my-articles-list"),
+	path('register/', auth_views.register, name="register"),
+	path('login/', auth_views.login_view , name="login"),
+	path('logout/', auth_views.logout_view, name="logout"),
+]
